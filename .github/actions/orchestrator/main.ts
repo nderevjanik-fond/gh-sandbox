7a6497aacae05401964df0e1642a3aba3fad5075
nick.derevjanik <nick.derevjanik@rewardgateway.com>
2025-04-17 16:35:42 -0700

    wip - orchestrator

--- .github/actions/orchestrator/main.ts
+++ .github/actions/orchestrator/main.ts
@@ -1,13 +1,20 @@
 import { Octokit } from '@octokit/rest';
+import { expandGlob } from 'jsr:@std/fs';
 
+const root = Deno.env.get('GITHUB_WORKSPACE');
 const token = Deno.env.get('GITHUB_TOKEN')!;
 const owner = Deno.env.get('OWNER')!;
-const repo = Deno.env.get('REPO')!.replace(`${owner}/`, '');
+const pattern = Deno.env.get('PATTERN');
 const pull_number = parseInt(Deno.env.get('PR')!, 10);
+const repo = Deno.env.get('REPO')!.replace(`${owner}/`, '');
 
 const octokit = new Octokit({
   auth: token,
 });
+
+for await (const file of expandGlob(`.github/workflows/${pattern}`, { root })) {
+  console.log(file.path);
+}
 
 const changedFiles = await octokit.paginate(
   octokit.rest.pulls.listFiles,
